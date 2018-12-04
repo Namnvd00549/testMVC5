8fbca49d9e99a140700c4f5ca155428a4cd08b5c
Namnvd00549 <namnvd00549@fpt.edu.vn>
2018-12-04 10:08:56 +0700

    decription

---
+++ TestMVC5/Context/ProductContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using TestMVC5.Models;
+
+namespace TestMVC5.Context
+{
+    public class ProductContext :DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ TestMVC5/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TestMVC5.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Subject { get; set; }
+        [Display(Name = "Start Time")]
+        [DataType(DataType.Time)]
+        public DateTime StartTime { get; set; }
+        [Display(Name = "Exam Date")]
+        [DataType(DataType.Date)]
+        public DataType ExamDate { get; set; }
+        [Display(Name = "Exam Durarion ")]
+        public int ExamDurarion { get; set; }
+        [Display(Name = "Class Room")]
+        public string ClassRoom { get; set; }
+        public string Faculty { get; set; }
+
+        public string Status { get; set; }
+    }
+}
